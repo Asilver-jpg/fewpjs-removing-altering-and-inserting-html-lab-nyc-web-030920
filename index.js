f3596f846ec9a37744c850436e7d127744b277ac
Asilver-jpg <asilver7493@gmail.com>
2020-04-21 11:37:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+let obj=document.getElementById("main");
+obj.remove();
+let newHeader= document.createElement('h1');
+newHeader.id= "victory";
+newHeader.innerHTML= "Alex Silver is the champion"
+
+document.body.appendChile(newHeader);
